8b7d6b8d36b9fc0bb4e9124c657825c6dc6a9621
Emmanuelomolaju <emmanuelomolaju964@gmail.com>
2023-12-28 18:26:14 +0100

    Revert "initial commit"
    This reverts commit 21ab297443ce314fc6d36c12345797305fa642fd.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import { BrowserRouter as Router, Switch } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home/Home";
-import { Route } from "react-router-dom/cjs/react-router-dom";
-import ItemPage from "./pages/Item/Item";
-import Footer from "./components/Footer";
 
+import { BrowserRouter as Router } from 'react-router-dom';
+import Hero from './views/Hero';
+import Section2 from './views/Section2';
+import Section3 from './views/Section3';
+import Section4 from './views/Section4';
+import Footer from './views/Footer';
 function App() {
+
   return (
     <Router>
-      <div className="App">
-        <Navbar />
-        <div className="mt-20 md:mt-36">
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/item/:itemSlug">
-              <ItemPage />
-            </Route>
-          </Switch>
-        </div>
-        <Footer />
-      </div>
+    <div className="App">
+        
+        <div className="content">
+          <Hero />
+          <Section2 />
+          <Section3 />
+          <Section4 />
+          <Footer />
+        </div> 
+    </div>
     </Router>
   );
 }
